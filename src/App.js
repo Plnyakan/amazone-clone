1b816d4634f0ea6612aff9044e78fcafb6ca3bec
plnyakan <plnyakan@student.wethinkcode.co.za>
2022-01-15 11:59:13 +0200

    added banner

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Header from './Componets/Header';
+import Home from './Componets/Home';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
      <Switch>
        <Route path="/checkout">
+       <Header />
          <h1>checkout</h1>
        </Route>
        <Route path="/login">
@@ -16,7 +18,7 @@ function App() {
        </Route>
        <Route path="/">
          <Header />
-         <h1>home</h1>
+         <Home/>
        </Route>
      </Switch>
 
